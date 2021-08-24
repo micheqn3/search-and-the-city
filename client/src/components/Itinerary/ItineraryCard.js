5f36f2a5b89ccf0a86e5eca58c0e33bbc78b8bf6
Michelle Nguyen <michellenguyen11239@aol.com>
2021-08-24 12:51:43 -0700

    Added itineraries.css and updated itineraryCard and itineraries.js

--- client/src/components/Itinerary/ItineraryCard.js
+++ client/src/components/Itinerary/ItineraryCard.js
@@ -7,11 +7,11 @@ const ItineraryCard = ( {item} ) => {
         <div className="col s12 m4 l4">
             <div className="card">
                 <div className="card-image">
-                    <a href="#"><img className="responsive-img itin-img" src="https://images.unsplash.com/photo-1500835556837-99ac94a94552?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80&h=1400"></img></a>
-                    <span className="card-title">{item.name}</span>
+                    <a href="#!"><img className="responsive-img itin-img" alt="Travel" src="https://images.unsplash.com/photo-1518557984649-7b161c230cfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80&h=1400"></img></a>
+                    <span className="card-title itin-card-title">{item.name}</span>
                 </div>
                 <div className="card-content">
-                    <a href="#"><i className="material-icons my-delete-icon">delete</i></a>
+                    <a href="#!"><i className="material-icons my-delete-icon">delete</i></a>
                 </div>
             </div>
         </div>
--- client/src/pages/Itineraries.js
+++ client/src/pages/Itineraries.js
@@ -1,20 +1,18 @@
 // Displays all of user's itineraries
 
-import React, { useState } from 'react';
+import React from 'react';
 import Auth from '../utils/auth';
 import { Redirect } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { GET_MY_ITINERARIES } from '../utils/queries';
+import ItineraryCard from '../components/Itinerary/ItineraryCard';
+import './itineraries.css';
 
 const Itineraries = () => {
-
-    // Set up initial state of itineraries
-    const [itineraries, setItineraries] = useState([]);
 
     // Set up query to retrieve user's itineraries
     const { data, loading } = useQuery(GET_MY_ITINERARIES);
     const userData = data?.myItineraries || {};
-    setItineraries(userData);
 
     // If user is not logged in, go to search page
     if (!Auth.loggedIn()) {
@@ -29,6 +27,8 @@ const Itineraries = () => {
         <section>
             <div className="row">
                 <h5 className="center-align">My Itineraries</h5>
+                {userData.length ? userData.map((item, index) => <ItineraryCard item={item} key={index} />)
+                : <h5 className="center-align">You don't have any itineraries.</h5>}
             </div>
         </section>
     ) 
